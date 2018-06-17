80bd32220da17115859ded240ed9015fc1e187df
CALICAWESOME <greendolphinavenue@gmail.com>
2018-06-17 18:11:06 -0400

    smol edits

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import WebSocket from 'ws';
 import EventEmitter from 'events';
 
-const serverURL = process.env.THZ_serverURL;
+// const serverURL = process.env.THZ_serverURL;
+const serverURL = 'bobby-mart.in';
 
-const socket: WebSocket = new WebSocket(`ws:${serverURL}:8080/?robot_num=1`);
+const socket: WebSocket = new WebSocket(`ws://${serverURL}:8080/?robot_num=1`);
 const emitter = new EventEmitter();
 socket.on('message', (data: string) => {
     const _data = JSON.parse(data);
@@ -21,7 +22,7 @@ emitter.on('DOF', onDOF);
 function onDOF(data: DOFToken): void {
     const motorVectors = [
         DOFMap.L.reduce((accum, val, index) => accum + val * data.DOF[index] / 2, 0),
-        DOFMap.R.reduce((accum, val, index) => accum + val * data.DOF[index] / 2, 0),
+        DOFMap.R.reduce((accum, val, index) => accum + val * data.DOF[index] / 2, 0)
     ];
     console.log(motorVectors)
 }
