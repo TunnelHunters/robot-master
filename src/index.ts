1dc422e3d151d3391407bf8bd68767cc94042286
CALICAWESOME <greendolphinavenue@gmail.com>
2018-07-09 00:10:04 -0400

    connect robot-master to video-slave

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
 import WebSocket from 'ws';
 import EventEmitter from 'events';
-import * as motorSlave from './lib/motor-control'
+import { VideoSlave } from './lib/video-slave'
 
 const serverURL = process.env.THZ_serverURL;
-
-const socket: WebSocket = new WebSocket(`ws://${serverURL}:8080/?robot_num=1`);
-const emitter = new EventEmitter();
-// MAIN MESSAGE LISTENER
-socket.on('message', (data: string) => {
-    const _data = JSON.parse(data);
-    emitter.emit(_data.type, _data);
-});
-
 const DOFMap = {
     // F/B, rotate
     L: [ 1,  1 ],
     R: [ 1, -1 ]
 };
 
-emitter.on('DOF', onDOF);
+// const socket: WebSocket = new WebSocket(`ws://${serverURL}:8080/?robot_num=1`);
+const emitter = new EventEmitter();
+const videoSlave = new VideoSlave();
+videoSlave.on('frame', (frame: Buffer) => {
+    console.log(frame.length);
+});
 
+// MAIN MESSAGE LISTENER
+// socket.on('message', (data: string) => {
+//     const _data = JSON.parse(data);
+//     emitter.emit(_data.type, _data);
+// });
+
+emitter.on('DOF', onDOF);
 function onDOF(data: DOFToken): void {
     const motorVectors: [number, number] = [
         DOFMap.L.reduce((accum, val, index) => accum + val * data.DOF[index] / 2, 0),
         DOFMap.R.reduce((accum, val, index) => accum + val * data.DOF[index] / 2, 0)
     ];
     console.log(motorVectors);
-    motorSlave.sendMotorVectors(motorVectors)
 }
---
+++ src/lib/video-slave.ts
@@ -0,0 +1,52 @@
+import { Socket, Server, createServer } from 'net'
+import { EventEmitter } from 'events'
+import { existsSync, unlinkSync } from 'fs'
+import exitHook from 'exit-hook'
+
+const socket_path = process.env.THZ_videoSocket;
+const endOfFrame = Buffer.from([0xFF, 0xD9]);
+
+exitHook(() => {
+    // delete socket file on exit
+    if (existsSync(socket_path))
+        unlinkSync(socket_path);
+
+    console.log(`Deleted ${socket_path}.`)
+});
+
+export class VideoSlave extends EventEmitter {
+    private socket: Socket;
+    private server: Server = createServer().listen(socket_path, () => console.log(`listening at ${socket_path}`));
+    private accumulator: Buffer = Buffer.alloc(0);
+
+    constructor() {
+        super();
+        this.server.on('connection', socket => {
+            console.log('Video salve connected!!');
+
+            socket.on('data', this.processFrameData.bind(this));
+            socket.on('close', hadError => {
+                console.log(`Connection to video slave closed${hadError ? ' with error' : ''}`);
+                this.socket = undefined;
+            });
+            this.socket = socket;
+        });
+    }
+
+    private processFrameData(data: Buffer): void {
+        const frameEndIndex = data.indexOf(endOfFrame);
+        // If there's an end of frame marker in the data,
+        if (frameEndIndex !== -1) {
+            // Concatenate the accumulator and everything in data up to (and including) the end of frame marker
+            this.emit('frame', Buffer.concat([this.accumulator, data.slice(0, frameEndIndex + 2)]));
+            // clear the accumulator
+            this.accumulator = Buffer.alloc(0);
+            // recursive call on the rest of the data
+            this.processFrameData(data.slice(frameEndIndex + 2, data.length));
+            return;
+        }
+        // Otherwise just add everything to the accumulator
+        this.accumulator = Buffer.concat([this.accumulator, data]);
+    }
+
+}
